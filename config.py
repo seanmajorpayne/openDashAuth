816ad1e80733fc709a22a21c9833a56358d8f695
Sean Payne <sean.payne.es@gmail.com>
2020-06-11 21:07:52 +0900

    adding email

--- config.py
+++ config.py
@@ -12,4 +12,4 @@ class Config(object):
 	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	ADMINS = ['seanmajorpayne@example.com']
+	ADMINS = ['example@example.com']
