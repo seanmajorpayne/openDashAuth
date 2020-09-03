332cf9b34ef8cdbd7a5d44d6c499cba189337c14
dazzag24 <dazzag@gmail.com>
2020-09-03 12:17:19 +0100

    Update __init__.py
    Added additional import for Python 3

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 import logging
+import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_bootstrap import Bootstrap
 
