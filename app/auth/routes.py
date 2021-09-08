d763644604634d3b6ddfd4c36b428213f04a50ba
seanmajorpayne <seanmajorpayne@gmail.com>
2021-09-08 16:04:11 +0900

    adding blueprints

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
-from app import app
+from app.auth import bp
 from app.forms import LoginForm
 from app.models import User
 from werkzeug.urls import url_parse
@@ -9,14 +9,15 @@ from werkzeug.urls import url_parse
 def get_user():
     return current_user
 
-@app.route('/')
-@app.route('/index')
+
+@bp.route('/')
+@bp.route('/index')
 @login_required
 def index():
     return render_template('index.html')
-	
-	
-@app.route('/login', methods=['GET', 'POST'])
+
+
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
     """
     Implements the login feature for the app.
@@ -27,30 +28,30 @@ def login():
 
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-		
+
     form = LoginForm()
-	
+
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first() # None if invalid
+        user = User.query.filter_by(username=form.username.data).first()  # None if invalid
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
-		
+
         login_user(user, remember=form.remember_me.data)
-		
+
         next_page = request.args.get('next')
-		
+
         """To prevent malicious users from adding a malicious site into the parameters,
         this checks to see if the url is relative.
         """
         if not next_page or url_parse(next_page).netloc != '' or next_page == '/logout':
             next_page = url_for('index')
         return redirect(next_page)
-	
+
     return render_template('login.html', form=form)
 
 
-@app.route('/logout')
+@bp.route('/logout')
 @login_required
 def logout():
     logout_user()
