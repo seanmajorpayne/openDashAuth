06d5f24f05034178176123e542d6e4be53a3d693
seanmajorpayne <seanmajorpayne@gmail.com>
2021-09-08 16:31:39 +0900

    small updates

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    JWT_SECRET_KEY = "super-secret"
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "DATABASE_URL"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
--- digitalDash.py
+++ digitalDash.py
@@ -1,6 +1,6 @@
 from app import create_app
 
 app = create_app()
-from app.plotlydash.dashboard import create_dashboard  # Do not move to top of file
+from app.plotlydash.dashboard import create_dashboard
 
 app = create_dashboard(app)
